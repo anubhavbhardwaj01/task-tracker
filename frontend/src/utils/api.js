8268bfcbbd2ee0a9ae1473690b289484920ddb4a
Anubhav <anubhavkumar5566@gmail.com>
2026-05-01 12:14:12 +0530

    updating env

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:4000/api",
+  baseURL: process.env.REACT_APP_API_URL || "https://optimistic-renewal-production-d43c.up.railway.app",
   headers: { "Content-Type": "application/json" },
 });
 
